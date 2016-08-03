e37afc0a5d7764ec9944f8d517830de007fa2644
Oleg Vasylkov <olehvasylkov@ukr.net>
2016-08-03 13:11:59 +0300

    2_logger

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -1,17 +1,22 @@
 package web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by filipenko_n on 03.08.2016.
  */
 public class UserServlet extends HttpServlet {
-
+    private static final Logger LOG = getLogger(UserServlet.class);
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
         response.sendRedirect("userList.jsp");
     }
--- src/main/webapp/userList.jsp
+++ src/main/webapp/userList.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>User list</title>
+    <title>Phone book</title>
 </head>
 <body>
 <h2><a href="index.html">Home</a></h2>
